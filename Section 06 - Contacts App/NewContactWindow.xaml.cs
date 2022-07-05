e179786fd7e9e2caa073e54601c746fba344d248
jamesacox53 <jamesacox53@gmail.com>
2022-07-05 21:00:35 +0100

    Completed Opening and Closing Windows

---
+++ Section 06 - Contacts App/NewContactWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Section_06___Contacts_App
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // code to save the contact
+
+            this.Close();
+        }
+    }
+}
