cb73f7c3e36645dc0ec9e5e5ba1452184e5947ee
jamesacox53 <jamesacox53@gmail.com>
2022-07-09 21:34:08 +0100

    Completed Update the ListView after closing the Window

---
+++ Section 06 - Contacts App/ContactDetailsWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Section_06___Contacts_App.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Section_06___Contacts_App
+{
+    /// <summary>
+    /// Interaction logic for ContactDetailsWindow.xaml
+    /// </summary>
+    public partial class ContactDetailsWindow : Window
+    {
+        private Contact contact;
+
+        public ContactDetailsWindow(Contact contact)
+        {
+            InitializeComponent();
+
+            this.contact = contact;
+            nameTextBox.Text = contact.Name;
+            emailTextBox.Text = contact.Email;
+            phoneTextBox.Text = contact.Phone;
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                contact.Name = nameTextBox.Text;
+                contact.Email = emailTextBox.Text;
+                contact.Phone = phoneTextBox.Text;
+
+                connection.CreateTable<Contact>();
+                connection.Update(contact);
+            }
+
+            this.Close();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Delete(contact);
+            }
+
+            this.Close();
+        }
+    }
+}
--- Section 06 - Contacts App/MainWindow.xaml.cs
+++ Section 06 - Contacts App/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace Section_06___Contacts_App
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Contact> contacts = new List<Contact>();
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -40,17 +42,77 @@ namespace Section_06___Contacts_App
 
         private void ReadDatabase()
         {
-            List<Contact> contacts;
-
             using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
             {
                 connection.CreateTable<Contact>();
-                contacts = connection.Table<Contact>().ToList();
+                contacts = connection.Table<Contact>().OrderBy(c => c.Name).ToList();
             }
 
             if (contacts == null) return;
 
             contactsListView.ItemsSource = contacts;
+        }
+
+        private void searchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            List<Contact> filteredList;
+
+            string text = searchTextBox.Text;
+
+            if (text == null || text == "")
+            {
+                filteredList = contacts;
+            }
+            else
+            {
+                filteredList = contacts.Where<Contact>(c => DoesContactContainText(c, text)).ToList();
+            }
+
+            contactsListView.ItemsSource = filteredList;
+        }
+
+        private bool DoesContactContainText(Contact contact, string text)
+        {
+            if (contact == null || text == null) return false;
+
+            string lowerCaseText = text.ToLower();
+
+            if (contact.Name != null)
+            {
+                string lowerCaseName = contact.Name.ToLower();
+                if (lowerCaseName.Contains(lowerCaseText)) return true;
+            }
+
+            if (contact.Email != null)
+            {
+                string lowerCaseEmail = contact.Email.ToLower();
+                if (lowerCaseEmail.Contains(lowerCaseText)) return true;
+            }
+
+            if (contact.Phone != null)
+            {
+                string lowerCasePhone = contact.Phone.ToLower();
+                if (lowerCasePhone.Contains(lowerCaseText)) return true;
+            }
+
+            return false;
+        }
+
+        private void contactsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Object selectedItem = contactsListView.SelectedItem;
+
+            if (selectedItem == null) return;
+
+            Contact selectedContact = (Contact) selectedItem;
+
+            if (selectedContact == null) return;
+
+            ContactDetailsWindow contactDetailsWindow = new ContactDetailsWindow(selectedContact);
+
+            contactDetailsWindow.ShowDialog();
+
+            ReadDatabase();
         }
     }
 }
